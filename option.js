17b4c017ddcbe0c971ca84635ddba87db2cf3791
菊池 大輔 <hero@kikuike-daisuke-no-MacBook-Pro-2.local>
2012-12-22 14:38:33 +0900

    option.js updated for date

--- option.js
+++ option.js
@@ -1,11 +1,11 @@
 var urls = JSON.parse(localStorage['urls']);
-//var urls = [{title:"title",url:"http://...",time:"12-22"},{title:"title2",url:"tekitou",time:"12-23"}].reverse();
+//var urls = [{title:"title",url:"http://...",time:"12-22"},{title:"title2",url:"tekitou",date:"12-23"}].reverse();
 for(var i = 0; i < urls.length; i++){
 	var newDt = document.createElement("dt");
 	newDt.innerHTML = "<a href=" + urls[i].url + ">" + urls[i].title + "</a>";
 	var newDd = document.createElement("dd");
 	newDd.innerHTML =  "<p class=\"url\">" + urls[i].url + "</p>";
-	newDd.innerHTML += "<p class=\"time\">" + urls[i].time + "</p>";
+	newDd.innerHTML += "<p class=\"date\">" + urls[i].date + "</p>";
 	var entries = document.getElementById('entries');
 	entries.appendChild(newDt);
 	entries.appendChild(newDd);	 
