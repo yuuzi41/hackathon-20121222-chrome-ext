7c556adb7c94fad8e8a3dadb68e2e84457b02467
菊池 大輔 <hero@kikuike-daisuke-no-MacBook-Pro-2.local>
2012-12-22 14:32:35 +0900

    option.js update

---
+++ option.js
@@ -0,0 +1,12 @@
+// var urls = localStorage['urls'];
+var urls = [{title:"title",url:"http://...",time:"12-22"},{title:"title2",url:"tekitou",time:"12-23"}].reverse();
+for(var i = 0; i < urls.length; i++){
+	var newDt = document.createElement("dt");
+	newDt.innerHTML = "<a href=" + urls[i].url + ">" + urls[i].title + "</a>";
+	var newDd = document.createElement("dd");
+	newDd.innerHTML =  "<p class=\"url\">" + urls[i].url + "</p>";
+	newDd.innerHTML += "<p class=\"time\">" + urls[i].time + "</p>";
+	var entries = document.getElementById('entries');
+	entries.appendChild(newDt);
+	entries.appendChild(newDd);	 
+}
